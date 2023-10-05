3d3bb5661769851782b74bcc968d1a676855cf28
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-10-05 12:42:23 -0300

    adicionando menu hamburguer

--- src/components/header/headerComponent.tsx
+++ src/components/header/headerComponent.tsx
@@ -1,18 +1,24 @@
 import "./style.css"
+import { useState } from "react";
 
 export default function Header() {
+    const [active, setMode] = useState(false);
+    const ToggleMode = () => {
+        setMode(!active)
+    }
     return (
         <header>
-            <div id="navBar" className="menu">
+            <div className={active ? "menu menuOpen" : "menu menuClose"}>
                 <ul id="navText">
+                    <hr />
                     <li>
-                        <a href="">pedro augusto</a>
+                        <a href="">Lorem</a>
                     </li>
                     <li>
-                        <a href="">pedro augusto</a>
+                        <a href="">Lorem</a>
                     </li>
                     <li>
-                        <a href="">pedro augusto</a>
+                        <a href="">Lorem</a>
                     </li>
                 </ul>
                 <div className="exit">
@@ -21,6 +27,9 @@ export default function Header() {
             </div>
 
             <div className="logotype">
+                <div className={active ? "icon iconActive" : "icon"} onClick={ToggleMode}>
+                    <div className="hamburguer hamburguerIcon"></div>
+                </div>
                 <a href="/">Callnet</a>
             </div>
         </header>
