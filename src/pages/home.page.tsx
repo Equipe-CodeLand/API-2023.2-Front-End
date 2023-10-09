fa33a660bdb483b06299cce8fe75fad5711b0a8f
Letícia <110743347+leticiacarvalho04@users.noreply.github.com>
2023-10-09 10:18:31 -0300

    arrumando a home

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -16,16 +16,6 @@ const Home: React.FC<HomeProps> = ({ data }) => {
     const link_title = ["Chamados", "Chamados ADM", "Cadastrar Usuário", "Forms Cadastro de Cliente"] // titulo para as paginas
     return(
         <div>
-            <Header 
-                link_0 = {link[0]} // Link para as páginas
-                link_1 = {link[1]}
-                link_2 = {link[2]}
-                link_3 = {link[3]}
-                link_title_0 = {link_title[0]} // titulo para as paginas
-                link_title_1 = {link_title[1]}
-                link_title_2 = {link_title[2]}
-                link_title_3 = {link_title[3]}
-            />
             <h1 className="text-tiny">Seja Bem-vindo(a) ao CallNet!</h1>
             <div>
                 <h4>Escolha o que deseja visualizar:</h4>
