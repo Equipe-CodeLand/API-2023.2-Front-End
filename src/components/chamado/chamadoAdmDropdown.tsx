e0562638755b2ead1c3e687d9463965e6ab533e4
Pedryn <ph9530338@gmail.com>
2023-10-11 21:19:51 -0300

    dropdown do adm

--- src/components/chamado/chamadoAdmDropdown.tsx
+++ src/components/chamado/chamadoAdmDropdown.tsx
@@ -1,6 +1,7 @@
 import './style.css'
 import { ChamadoAdmDetalhes } from './chamadoAdm.interface'
 import { useState } from 'react'; // Importe o useState do React
+import axios from 'axios';
 
 
 function ChamadoAdmDropdown(props: any) {
@@ -13,13 +14,14 @@ function ChamadoAdmDropdown(props: any) {
     const fecharPopup = () => {
         setMostrarPopup(false);
     }
+    
 
     if (props.open) {
         let conversa = props.conversa.map((msg: ChamadoAdmDetalhes) => {
             return (
                 <>
                     <div className='conteudo-adm'>
-                        <p className='email-cliente'>{'Contato: ' + msg.email}</p>
+                        <p>{msg.email}</p>
                         <h2>{msg.nome + ' - Cliente'}</h2>
                         <p className='texto-adm'>{msg.msg}</p>
 
@@ -34,61 +36,65 @@ function ChamadoAdmDropdown(props: any) {
                 <div className="chamado-dropdown">
                     {conversa}
                     {mostrarPopup && (
-                        <div className="popup">
-                            <table className="atendente-table">
-                                <thead>
-                                    <tr>
-                                    <th>Atendente</th>
-                                    <th>Chamados</th>
-                                    <th>Atribuir Atendente</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    <td className="atendente">Lucas Almeida</td>
-                                    <td>0 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde" >Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                    <tr>
-                                    <td>Matias Pereira</td>
-                                    <td>1 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde">Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                    <tr>
-                                    <td>Daniel Costa</td>
-                                    <td>2 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde">Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                    <tr>
-                                    <td>Felipe Ribeiro</td>
-                                    <td>3 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde">Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                    <tr>
-                                    <td>Rafaela Costa</td>
-                                    <td>4 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde">Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                    <tr>
-                                    <td>Isadora Barbosa</td>
-                                    <td>4 Chamado(s) ativo(s) no momento</td>
-                                    <td>
-                                        <button className="botao-verde">Atribuir atendente</button>
-                                    </td>
-                                    </tr>
-                                </tbody>
-                                </table>
-                            <button className='fechar_btn' onClick={fecharPopup}>Fechar Pop-up</button>
+                        <div id='meu-modal' className="modal">
+                            <div className="modal-content">
+                                <br></br>
+                                <table className="atendente-table">
+                                    <thead>
+                                        <tr>
+                                        <th>Atendente</th>
+                                        <th>Chamados</th>
+                                        <th>Atribuir Atendente</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <td>Lucas Almeida</td>
+                                        <td>0 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde" >Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>Matias Pereira</td>
+                                        <td>1 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde">Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>Daniel Costa</td>
+                                        <td>2 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde">Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>Felipe Ribeiro</td>
+                                        <td>3 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde">Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>Rafaela Costa</td>
+                                        <td>4 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde">Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                        <tr>
+                                        <td>Isadora Barbosa</td>
+                                        <td>4 Chamado(s) ativo(s) no momento</td>
+                                        <td>
+                                            <button className="botao-verde">Atribuir atendente</button>
+                                        </td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                    <br></br>
+                                <button className='fechar_btn' onClick={fecharPopup}>Fechar</button>
+                            </div>
                         </div>
                     )}
                 </div>
