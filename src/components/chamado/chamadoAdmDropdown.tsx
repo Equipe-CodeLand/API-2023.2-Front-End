a55459e46160eefbd402190a2781dfd3dd92b629
Pedryn <ph9530338@gmail.com>
2023-10-26 07:32:38 -0300

    frontend do atribuir atendente

--- src/components/chamado/chamadoAdmDropdown.tsx
+++ src/components/chamado/chamadoAdmDropdown.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChamadoAdmDetalhes } from './chamadoAdm.interface';
 import axios from 'axios'; // Importe o axios
 import './style.css';
+import Atendente from './Atendente.interface';
 
 function ChamadoAdmDropdown(props: any) {
     const [mostrarPopup, setMostrarPopup] = useState(false); // Estado para controlar a exibição do pop-up
@@ -14,9 +15,27 @@ function ChamadoAdmDropdown(props: any) {
         setMostrarPopup(false);
     }
 
+    function buscarAtendentes() {
+        axios.get(`http://localhost:5000/atendentes`,)
+            .then(res => {
+                let atendentes = res.data.map((atendente:any) => {
+                    return {
+                        id: atendente.id,
+                        nome: atendente.usuario.nome + ' ' + atendente.usuario.sobrenome,
+                    }
+                })
+                setAtendentes(atendentes)
+        }) 
+    }
+
+    const [atendentes, setAtendentes] = useState<Atendente[]>([])
+    useEffect(() => {
+        buscarAtendentes()
+    }, [])
+
     const atribuirAtendente = (atendenteId: number) => {
         // Aqui você pode usar o axios para fazer uma requisição
-        axios.post(`http://localhost:5000/atribuirAtendente/${atendenteId}`, { chamadoId: props.chamadoId })
+        axios.post(`http://localhost:5000/atribuirAtendente`, { chamadoId: props.chamadoId, atendenteId: atendenteId })
             .then(response => {
                 // Atualize o estado ou faça qualquer outra ação necessária
                 console.log("Atendente atribuído com sucesso!");
@@ -51,20 +70,24 @@ function ChamadoAdmDropdown(props: any) {
                                 <table className="atendente-table">
                                     <thead>
                                         <tr>
-                                            <th>Atendente</th>
-                                            <th>Chamados</th>
+                                            <th>Id</th>
+                                            <th>Atendentes</th>
                                             <th>Atribuir Atendente</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>Lucas Almeida</td>
-                                            <td>0 Chamado(s) ativo(s) no momento</td>
-                                            <td>
-                                                <button className="botao-verde" onClick={() => atribuirAtendente(1)}>Atribuir atendente</button>
-                                            </td>
-                                        </tr>
-                                        {/* (outros atendentes) */}
+                                        {atendentes.map(atendente=> {
+                                            return (
+                                                <tr>
+                                                    <td>{atendente.id}</td>
+                                                    <td>{atendente.nome}</td>
+                                                    <td>
+                                                        <button className="botao-verde" onClick={() => atribuirAtendente(atendente.id)}>Atribuir atendente</button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                           
+                                        } )}
                                     </tbody>
                                 </table>
                                 <br></br>
