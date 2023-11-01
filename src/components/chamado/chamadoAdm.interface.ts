6a1326269cf10e319a246476795b9ee2e9707190
Letícia <110743347+leticiacarvalho04@users.noreply.github.com>
2023-11-01 18:58:33 -0300

    atualizando as pages usando o back

--- src/components/chamado/chamadoAdm.interface.ts
+++ src/components/chamado/chamadoAdm.interface.ts
@@ -1,4 +1,5 @@
 export interface ChamadoAdm {
+    id: number,
     nome: string,
     tema: string,
     status: {
--- src/pages/chamadosAdm.page.tsx
+++ src/pages/chamadosAdm.page.tsx
@@ -1,46 +1,86 @@
-import ChamadoAdmComponent from "../components/chamado/chamadoAdmComponent"
-import {ChamadoAdm} from "../components/chamado/chamadoAdm.interface"
-import Header from "../components/header/headerComponent"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../components/header/headerComponent";
+import ChamadoAdmComponent from "../components/chamado/chamadoAdmComponent";
+import { ChamadoAdm } from "../components/chamado/chamadoAdm.interface";
+import ChamadoAdmDropdown from "../components/chamado/chamadoAdmDropdown";
 
 export default function ChamadosAdm() {
-    let chamados: Array<ChamadoAdm> = [
-        {
-            nome: 'Alice Nunes', tema: 'Problema de Conexão', status: { id: '3', texto: 'Não iniciado' }, prioridade: { id: 3, value: "Baixa" }, hora: '26/09/2023 00:59', conversa: [{nome: 'Alice Nunes', email: 'alicenunes@gmail.com', msg: 'Nas últimas duas semanas, tenho notado ruídos estranhos durante minhas chamadas telefônicas. Esses ruídos tornaram difícil manter conversas claras e consistentes com meus amigos e familiares. Além disso, houve ocasiões em que as chamadas simplesmente caíram de forma inesperada, o que tem sido bastante frustrante.'}]
-        },       
-        {
-            nome: 'Leonardo Pereira', tema: 'Qualidade de linha', status: { id: '3', texto: 'Não iniciado' }, prioridade: { id: 2, value: "Média" }, hora: '26/09/2023 00:59', conversa: [{nome: 'Leonardo Pereira', email: 'leopereira@gmail.com', msg: 'Nas últimas duas semanas, tenho notado ruídos estranhos durante minhas chamadas telefônicas. Esses ruídos tornaram difícil manter conversas claras e consistentes com meus amigos e familiares. Além disso, houve ocasiões em que as chamadas simplesmente caíram de forma inesperada, o que tem sido bastante frustrante.'}]
-        },
-        {
-            nome: 'Luís Oliveira', tema: 'Problemas com fatura', status: { id: '3', texto: 'Não iniciado' }, prioridade: { id: 1, value: "Alta" }, hora: '26/09/2023 00:59', conversa: [{nome: 'Luís Oliveira', email: 'luisoliveira@gmail.com', msg: 'Nas últimas duas semanas, tenho notado ruídos estranhos durante minhas chamadas telefônicas. Esses ruídos tornaram difícil manter conversas claras e consistentes com meus amigos e familiares. Além disso, houve ocasiões em que as chamadas simplesmente caíram de forma inesperada, o que tem sido bastante frustrante.'}]
-        },
-    ]
+  const [chamados, setChamados] = useState<ChamadoAdm[]>([]);
 
+  function buscarChamados() {
+    const inicio = new Date();
+    axios.get(`http://localhost:5000/chamados`)
+      .then(res => {
+        console.log(res);
+        let chamados = res.data.map((c: any) => {
+          let nomeCliente = c.cliente && c.cliente.usuario && c.cliente.usuario.nome;
+          let sobrenomeCliente = c.cliente && c.cliente.usuario && c.cliente.usuario.sobrenome;
+        
+          return {
+            id: c.id,
+            nome: (nomeCliente && sobrenomeCliente) ? nomeCliente + ' ' + sobrenomeCliente : '',
+            tema:{
+              id: c.tema.id,
+              texto: c.tema.nome
+            },
+            status: {
+              id: c.status.id,
+              texto: c.status.nome
+            },
+            prioridade:{
+              id: c.prioridade.id,
+              value: c.prioridade.nome
+            },
+            hora: inicio,
+            fim: c.final
+          }                
+        })
+        console.log(chamados);
+        setChamados(chamados);
+      });
+  }
 
-    let list = chamados.map((chamado: any) => {
-        return <ChamadoAdmComponent
-            nome={chamado.nome}
-            tema={chamado.tema}
-            status={chamado.status}
-            prioridade={chamado.prioridade}
-            hora={chamado.hora}
-            conversa={chamado.conversa}
-        ></ChamadoAdmComponent>
-    })
+  useEffect(() => {
+    buscarChamados();
+  }, []);
 
-    const link = ["/", "/", "/cadastroUser"] // Link para as páginas
-    const link_title = ["Relatórios", "Chamadas em Aberto", "Cadastrar Usuário"] // titulo para as paginas
+  const link = ["/", "/", "/cadastroUser"]; // Link para as páginas
+  const link_title = ["Relatórios", "Chamadas em Aberto", "Cadastrar Usuário"]; // titulo para as paginas
 
-    return (
+  return (
+    <div>
+      <Header
+        link_0={link[0]} 
+        link_1={link[1]}
+        link_2={link[2]}
+        link_title_0={link_title[0]} 
+        link_title_1={link_title[1]}
+        link_title_2={link_title[2]}
+      />
+      {chamados.length > 0 && (
         <div>
-            <Header
-                link_0 = {link[0]} // Link para as páginas
-                link_1 = {link[1]}
-                link_2 = {link[2]}
-                link_title_0 = {link_title[0]} // titulo para as paginas
-                link_title_1 = {link_title[1]}
-                link_title_2 = {link_title[2]}
-            />
-            {list}
+          {chamados.map(chamado => {
+          return (
+            <div key={'chamado' + chamado.id}>
+              <ChamadoAdmComponent
+                id={chamado.id}
+                nome={chamado.nome}
+                tema={chamado.tema}
+                status={chamado.status}
+                prioridade = {chamado.prioridade}
+                hora={new Date(chamado.hora).toLocaleDateString() + " - " + new Date(chamado.hora).toLocaleTimeString()}
+                conversa={chamado.conversa ? chamado.conversa : []}
+                key={'chamado' + chamado.id}
+              />
+              {chamado.conversa && chamado.conversa.length > 0 && (
+                <ChamadoAdmDropdown open={true} conversa={chamado.conversa} /> 
+              )}
+            </div>
+          )
+        })}
         </div>
-    )
+      )}
+    </div>
+  );
 }
