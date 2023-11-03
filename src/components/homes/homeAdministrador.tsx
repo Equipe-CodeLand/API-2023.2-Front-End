e1d8f100f46e8113056ea054e4cadd3ca04e2fbb
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-11-03 20:24:02 -0300

    finalizando tela home do administrador

--- src/components/homes/homeAdministrador.tsx
+++ src/components/homes/homeAdministrador.tsx
@@ -3,7 +3,18 @@ import "./styles/administrador.css";
 export default function Home() {
     return (
         <div className="homeAdm">
-            <h1>Home Page Administrador</h1>
+            <section className="conteudo">
+                <div>
+                    <div className="titulo">
+                        <h1 id="tituloPrincipal">Bem vindo ao <strong>Callnet</strong>!</h1>
+                        <p>Monitore e organize seus atendentes!</p>
+                    </div>
+                    <div className="chamados">
+                        <div className=""><a href="/chamados/Adm">Acessar chamados</a></div>
+                        <div className=""><a href="/cadastroUser">Cadastrar novo Usuário</a></div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
