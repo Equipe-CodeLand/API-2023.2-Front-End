795ca0206d6d648e14df8759b549e4a91fe487f9
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-11-08 22:00:57 -0300

    normalizando css da parte do atendente

--- src/pages/chamadoAte.page.tsx
+++ src/pages/chamadoAte.page.tsx
@@ -46,18 +46,16 @@ export default function ChamadosAdm() {
     buscarChamados();
   }, []);
 
-  const link = ["/home/atendente", "/chamadosAte", "/meusChamados/atendente"]; // Link para as páginas
-  const link_title = ["Home", "Chamadas em Aberto", "Meus chamados"]; // titulo para as paginas
+  const link = ["/home/atendente", "/meusChamados/atendente"]; // Link para as páginas
+  const link_title = ["Home", "Meus chamados"]; // titulo para as paginas
 
   return (
     <div>
       <Header
         link_0={link[0]} 
         link_1={link[1]}
-        link_2={link[2]}
         link_title_0={link_title[0]} 
         link_title_1={link_title[1]}
-        link_title_2={link_title[2]}
       />
       {chamados.length > 0 && (
         <div>
